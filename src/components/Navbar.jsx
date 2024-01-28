622fd8bd3018bbd5b09b59c29028fb951e13667a
heopoint <36278561+heopoint@users.noreply.github.com>
2024-01-28 17:50:01 +0900

    7번째 상품상세페이지 완료

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FaShopify } from "react-icons/fa";
 import { RiAdminLine } from "react-icons/ri";
-import { login,logout,onUserStateChange} from '../api/firebase';
+import { login, logout, onUserStateChange } from '../api/firebase';
 import User from './User';
 import Button from './ui/Button';
 
@@ -12,7 +12,7 @@ export default function Navbar() {
 
   useEffect(() => {
     // onUserStateChange(setUser);
-    onUserStateChange(user=>{
+    onUserStateChange(user => {
       console.log(user)
       setUser(user)
     });
@@ -20,30 +20,60 @@ export default function Navbar() {
     console.log(user)
   }, []);
 
- 
+
 
   return (
     <header className=' border-b border-gray-300 p-2'>
       <div className='max-w-1260 mx-auto flex justify-between'>
-          <Link to='/' className='flex items-center text-2xl text-brand'>
-        <FaShopify />
-        <h1>라운지비</h1>
-      </Link>
-      <nav className='flex items-center gap-4 font-semibold'>
-        <Link to='/products'>Products</Link>
-        {user && <Link to='/carts'>Carts</Link>}
-        
-        {user && user.isAdmin && (
-          <Link to='/products/new' className='text-2xl'>
-              <RiAdminLine />
-          </Link>
-        )}
+        <Link to='/' className='flex items-center text-2xl text-brand'>
+          <FaShopify />
+          <h1>라운지비</h1>
+        </Link>
+        <nav className='flex items-center   font-semibold'>
+          {!user && (
+            <ul className='hidden lg:flex   items-center gap-8 font-semibold '>
+              <li><Link to='/products'>BRAND</Link></li>
+              <li><Link to='/products'>Review AI</Link></li>
+              <li><Link to='/products'>NEW -10%</Link></li>
+              <li><Link to='/products'>BEST</Link></li>
+              <li><Link to='/products'>APPAREL</Link></li>
+              <li><Link to='/products'>BAG/SHOES</Link></li>
+              <li  className='mr-8'><Link to='/products'>BEAUTY</Link></li>
+              <li className='font-medium  text-cyan-700'><Link to='/products'>OUTLET 기획전</Link></li>
+              <li  className='font-medium  text-cyan-700'><Link to='/products'>이벤트</Link></li>
+            </ul>
+          )}
 
-        {user && <User user={user} />}
-        {!user && <Button text={'Login'} onClick={login} />}
-        {user && <Button text={'Logout'} onClick={logout} />}
-      </nav>
-      </div>
+        </nav>
+
+        <div className='gnb flex items-center gap-8 font-semibold'>
+
+          {user && <Link to='/carts'>Carts</Link>}
+
+          {user && user.isAdmin && (
+            <Link to='/products/new' className='text-2xl'>
+              <RiAdminLine />
+            </Link>
+          )}
+
+          {user && <User user={user} />}
+          {!user && <Button text={'Login'} onClick={login} />}
+          {user && <Button text={'Logout'} onClick={logout} />}
+
+        </div>
+        </div>
     </header>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
